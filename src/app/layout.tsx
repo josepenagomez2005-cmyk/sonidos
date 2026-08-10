0b3e2eeae806c2fe0e0b6b74ebb021a901000b8e
Jose Pena <josepenagomez2005@gmail.com>
2026-08-10 18:32:45 -0400

    Agregar Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import {Analytics} from "@vercel/analytics/react";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -16,7 +17,7 @@ export default function RootLayout({
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
-      <body className="bg-[#f5efe6]">{children}</body>
+      <body className="bg-[#f5efe6]">{children} <Analytics/> </body>
     </html>
   );
 }
